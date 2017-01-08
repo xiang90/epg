7a5822921ee5eb5a0d8877acea468b7066eed8e6
Xiang Li <xiangli.cs@gmail.com>
2017-01-08 13:58:34 -0800

    sync: keep revision option

--- sync.go
+++ sync.go
@@ -10,27 +10,49 @@ import (
 )
 
 type dbSyncer struct {
-	table string
+	table       string
+	noOverwrite bool
 
 	src  mirror.Syncer
 	dest *sql.DB
 }
 
 func (ds *dbSyncer) sync(ctx context.Context) error {
-	stmt, err := ds.dest.Prepare(fmt.Sprintf("INSERT INTO %s(key,value) VALUES ($1,$2) ON CONFLICT (key) DO UPDATE SET value=$2", ds.table))
-	if err != nil {
-		return err
-	}
+	var (
+		stmt  *sql.Stmt
+		dstmt *sql.Stmt
 
-	dstmt, err := ds.dest.Prepare(fmt.Sprintf("DELETE FROM %s WHERE value=$1", ds.table))
-	if err != nil {
-		return err
+		err error
+	)
+
+	if ds.noOverwrite {
+		stmt, err = ds.dest.Prepare(fmt.Sprintf("INSERT INTO %s(key,event,value,revision) VALUES ($1,$2,$3,$4)", ds.table))
+		if err != nil {
+			return err
+		}
+		dstmt = stmt
+	} else {
+		stmt, err = ds.dest.Prepare(fmt.Sprintf("INSERT INTO %s(key,value) VALUES ($1,$2) ON CONFLICT (key) DO UPDATE SET value=$2", ds.table))
+		if err != nil {
+			return err
+		}
+
+		dstmt, err = ds.dest.Prepare(fmt.Sprintf("DELETE FROM %s WHERE value=$1", ds.table))
+		if err != nil {
+			return err
+		}
 	}
 
 	rc, errc := ds.src.SyncBase(ctx)
 	select {
 	case r := <-rc:
 		for _, kv := range r.Kvs {
+			if ds.noOverwrite {
+				if _, err := stmt.Exec(kv.Key, clientv3.EventTypePut.String(), kv.Value, kv.ModRevision); err != nil {
+					return err
+				}
+				continue
+			}
 			if _, err := stmt.Exec(kv.Key, kv.Value); err != nil {
 				return err
 			}
@@ -46,6 +68,13 @@ func (ds *dbSyncer) sync(ctx context.Context) error {
 		}
 
 		for _, e := range wr.Events {
+			if ds.noOverwrite {
+				if _, err := stmt.Exec(e.Kv.Key, e.Type.String(), e.Kv.Value, e.Kv.ModRevision); err != nil {
+					return err
+				}
+				continue
+			}
+
 			switch e.Type {
 			case clientv3.EventTypePut:
 				if _, err := stmt.Exec(e.Kv.Key, e.Kv.Value); err != nil {
